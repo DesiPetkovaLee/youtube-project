63e5d55496ea766de7c77cf2003f93b28adc43b6
Desi Lee <71154350+DesiPetkovaLee@users.noreply.github.com>
2023-12-06 19:44:09 +0000

    logo change

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,6 @@
 import { Stack } from "@mui/material";
 import { Link } from "react-router-dom";
-
-import { logo } from "../utils/constants";
+import logo from "./youtubelogo.png";
 import SearchBar from "./SearchBar";
 
 const Navbar = () => (
